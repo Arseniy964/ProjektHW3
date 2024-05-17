112f69ec971af1af2af74aa1ef5b403fc3df0182
Your Name <s6nyi8@gmail.com>
2024-05-17 14:36:57 +0600

    New Cod

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,40 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        BankAccount account = new BankAccount();
+//        account.deposit(15000);
+//        while (true){
+//            try {
+//                account.withDraw(6000);
+//                System.out.println("Снято 6000 сом. Остаток на счете: " + account.getAmount());
+//            }catch (LimitException ex){
+//                ex.printStackTrace();
+//            }
+//            System.out.println("Остаток на счете: " + account.getAmount());
+//            break;
+//        }
+//    }
+//}
 public class Main {
     public static void main(String[] args) {
         BankAccount account = new BankAccount();
         account.deposit(15000);
-        while (true){
+
+        while (true) {
             try {
                 account.withDraw(6000);
                 System.out.println("Снято 6000 сом. Остаток на счете: " + account.getAmount());
-            }catch (LimitException ex){
-                ex.printStackTrace();
+            } catch (LimitException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Снимаем остаток: " + e.getRemainingAmount());
+                try {
+                    account.withDraw((int) e.getRemainingAmount());
+                    System.out.println("Снято: " + e.getRemainingAmount());
+                } catch (LimitException ex) {
+                    ex.printStackTrace();
+                }
+                System.out.println("Остаток на счете: " + account.getAmount());
+                break;
             }
-            System.out.println("Остаток на счете: " + account.getAmount());
-            break;
         }
     }
 }
